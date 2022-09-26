03b100a2183d4bacd31cfbe115b790177d9ecea2
marcos <msanchezm@iesch.org>
2022-09-26 17:32:03 +0200

    26/09/2022 -PUTO PERRO ARREGLADO

--- app/src/main/java/org/iesch/mi_edad_canina/MainActivity.java
+++ app/src/main/java/org/iesch/mi_edad_canina/MainActivity.java
@@ -7,41 +7,48 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.iesch.mi_edad_canina.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       // setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
         // findViewById es para enlazar los elementos del XML con los del .java
-        EditText ageEdit = findViewById(R.id.Edad_EditText); // R porque es un registro de android donde se guardan todas las variables
-        Button btnCalculate = findViewById(R.id.Calcular_Btn);
-        TextView txtViewResult = findViewById(R.id.Respuesta_TextView);
+       // EditText ageEdit = findViewById(R.id.Edad_EditText); // R porque es un registro de android donde se guardan todas las variables
+       // Button btnCalculate = findViewById(R.id.Calcular_Btn);
+       // TextView txtViewResult = findViewById(R.id.Respuesta_TextView);
 
-
+        // Para avusos en la pestaña de logcat
+        /*
         Log.d("DAM", "MainActivity cargada con éxito");
         Log.e("DAM", "MainActivity cargada con éxito");
         Log.w("DAM", "MainActivity cargada con éxito");
         Log.i("DAM", "MainActivity cargada con éxito");
         Log.v("DAM", "MainActivity cargada con éxito");
+         */
 
-        btnCalculate.setOnClickListener(new View.OnClickListener() {
+        binding.CalcularBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Al hacer click queremos capturar el texto introducido
 
-                String age = ageEdit.getText().toString();
+                String age = binding.EdadEditText.getText().toString();
                 if (!age.isEmpty()) {
                     int ageInteger = Integer.parseInt(age);
                     int result = ageInteger * 7;
                     //String textResult = "If you were a dog your age would be " + result + " years old";
                     String textResult = getString(R.string.result_format, result);
 
-                    txtViewResult.setText(textResult);
+                    //  txtViewResult.setText(textResult);
+    binding.RespuestaTextView.setText(textResult);
                 } else {
                     Log.d("MainActivity", "El campo edad esta vacío");
                     //Toast.makeText(MainActivity.this, "Age field is empty", Toast.LENGTH_SHORT).show();
